fa46caa9fc304d2cff62107d5480e4003ed3c131
Michele Stassen <m.a.stassen@student.tue.nl>
2020-04-22 18:41:28 +0200

    Parsing using d3.js
    Converted the parsing to d3.js to make use of the built-in d3.nest function for grouping

--- upload.js
+++ upload.js
@@ -6,68 +6,36 @@ form.addEventListener('submit', e => {
 	e.preventDefault()
 
 	const file = document.querySelector('[type=file]').files[0];
+	const reader = new FileReader();
 
-	const formData = new FormData();
+  // d3.tsv reads csv (commas) but with tabs from an URL
+	// since our file is uploaded by the user we don't have an URL
+	// below is a workaround for this, which loads it during the callback
+	// can't seem to move it to separate function call
+	reader.addEventListener("load", function() {
+			d3.tsv(reader.result).then(function(data) {
+				data.forEach(function(d) {
+					d.Timestamp = +d.Timestamp;
+					d.FixationIndex = +d.FixationIndex;
+					d.FixationDuration = +d.FixationDuration;
+					d.MappedFixationPointX = +d.MappedFixationPointX;
+					d.MappedFixationPointY = +d.MappedFixationPointY;
+				});
 
-	// reads .csv as text
-	var reader = new FileReader();
-	reader.readAsText(file, 'UTF-8');
+				groupingStimuli(data);
+	}, false);
 
-	reader.onload = readerEvent => {
-		var content = readerEvent.target.result;
-		processCSV(content);
+	if (file) {
+		reader.readAsDataURL(file);
 	}
 })
 
-// splits the tabs of the text file into array elements
-function processCSV(allText) {
-	var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split('	');
-    var lines = [];
-
-    for (var i = 1; i < allTextLines.length; i++) {
-        var data = allTextLines[i].split('	');
-        if (data.length == headers.length) {
-            var tarr = [];
-            for (var j = 0; j < headers.length; j++) {
-                tarr.push(headers[j] + ":" + data[j]);
-            }
-            lines.push(tarr);
-        }
-    }
-	parseCSV(lines);
-}
-
-// sorts the array elements to be grouped by same StimuliName
-// if they have the same StimuliName they are added to grouped
-// if not, then grouped is added to groupedArray
-function parseCSV(arrayText) {
-	var prevMap = "NULL";
-	var groupedArray = [];
-	var sameStimuli = [];
-	var j = 0;
-
-	for (var i = 0; i < arrayText.length; i++) {
-		if (arrayText[i][1] == prevMap) {
-			var temp = [];
-			for (var k = 0; k < arrayText[i].length; k++) {
-                temp.push(arrayText[i][k]);
-            }
-			sameStimuli.push(temp);
-		}
-		else if (arrayText[i][1] !== prevMap) {
-			if (sameStimuli.length !== 0) {
-				groupedArray[j] = sameStimuli;
-				sameStimuli = [];
-				j++;
-			}
-			var temp = [];
-            for (var k = 0; k < arrayText[i].length; k++) {
-                temp.push(arrayText[i][k]);
-            }
-			sameStimuli.push(temp);
-			prevMap = arrayText[i][1];
-		}
-	}
-	console.log(groupedArray);
+// groupedByStimuli is an array of objects. each object has the
+// StimuliName as a key and the others are values
+// can use this for counting, means, sums, multi-level indexing
+function groupingStimuli(data) {
+	var groupedByStimuli = d3.nest()
+		.key(function(d) { return d.StimuliName; })
+		.entries(data);
+	console.log(groupedByStimuli)
 }
