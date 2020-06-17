ad758f7e5e63d5534a9dc915b69e1d58bab1389a
JennieAV <64147614+JennieAV@users.noreply.github.com>
2020-06-17 10:34:11 +0200

    sidebar upgrades

--- visualizations.php
+++ visualizations.php
@@ -1,4 +1,16 @@
     <div class="sidebar">
+
+        <p class="devider">User Selection</p>
+
+        <div class="slidecontainer">
+        <div class="buttoncontainer" id="highlighted_user">
+            <p>Click on user to highlight</p>
+            <p>Click again to undo</p>
+        </div>
+        </div>
+
+        
+        <p class="devider">Scanpath</p>
         <div class="slidecontainer">
             <div class="left_elem" id="base_stroke_width">
                  <p>Base path width:<div data-tooltip="Increase or decrease the width of the connections between fixations">❓</div></p> 
@@ -8,9 +20,7 @@
                 <p>Highlight Path Width:<div data-tooltip="Increase or decrease the width of highlighted paths for better clarity">❓</div></p>
                 <input type="range" min="1" max="10" value="3" class="slider" id="highlight_stroke_width_slider">
             </div>
-        </div>
-
-        <div class="slidecontainer">
+        
             <div class="left_elem" id="base_stroke_opacity">
                 <p>Base Path Opacity<div data-tooltip="Decrease if necessary to see both the background picture and the paths">❓</div></p>
                 <input type="range" min="0" max="10" value="7" class="slider" id="base_stroke_opacity_slider">
@@ -19,9 +29,7 @@
                 <p>Highlight Path Opacity:<div data-tooltip="Decrease if necessary to see both the background picture and the highlighted paths">❓</div></p>
                 <input type="range" min="0" max="10" value="10" class="slider" id="highlight_stroke_opacity_slider">
             </div>
-        </div>
-
-        <div class="slidecontainer">
+        
             <div class="left_elem" id="base_fixation_radius">
                 <p>Base Fixation Size:<div data-tooltip = "Increase or decrease the size of individual fixations">❓</div></p>
                 <input type="range" min="0" max="15" value="3" class="slider" id="base_fixation_radius_slider">
@@ -30,9 +38,7 @@
                 <p>Highlight Fixation Size:<div data-tooltip = "Increase the size of individual highlighted fixations">❓</div></p>
                 <input type="range" min="0" max="15" value="6" class="slider" id="highlight_fixation_radius_slider">
             </div>
-        </div>
-
-        <div class="slidecontainer">
+                
             <div class="left_elem" id="base_fixation_opacity">
                 <p>Base Fixation Opacity:<div data-tooltip= "Decrease if necessary to see both the background picture and the fixations">❓</div></p>
                 <input type="range" min="0" max="10" value="7" class="slider" id="base_fixation_opacity_slider">
@@ -41,22 +47,16 @@
                 <p>Highlight Fixation Opacity:<div data-tooltip = "Decrease if necessary to see both the background picture and the highlighted fixations">❓</div></p>
                 <input type="range" min="0" max="10" value="10" class="slider" id="highlight_fixation_opacity_slider">
             </div>
+
+            <div class="buttoncontainer" id = "base_colours">
+                <p>Select base path and fixation color:</p>
+                <input type="button" id = "dark_blue_base" value = "Blue" name="base_colour">
+                <input type="button" id = "black_base" value = "Black" name="base_colour">
+            </div>
         </div>
+        
 
-        <div class="buttoncontainer" id = "base_colours">
-            <p>Select base path and fixation color:</p>
-            <input type="button" id = "dark_blue_base" value = "Blue" name="base_colour">
-            <input type="button" id = "black_base" value = "Black" name="base_colour">
-        </div>
-
-        <div class="buttoncontainer" id="highlighted_user">
-            <p> User selection here is linked to the heat map. </p>
-            <p>Click on user to highlight</p>
-            <p>Click again to undo</p>
-        </div>
-
-
-
+        <p class="devider">Bubblemap</p>
         <div class="slidecontainer">
             <div id="grid_size">
                 <p> Change grid size: <div data-tooltip = "Increase for more and smaller bubbles, decrease for less and bigger bubbles">❓</div></p>
@@ -64,7 +64,7 @@
             </div>
         </div>
 
-
+        <p class="devider">Heatmap</p>
 
         <div class="slidecontainer" id="all_heatmap_sliders">
             <div id="intensity_heatmap">
@@ -81,18 +81,18 @@
             </div>
             <p> Checking the checkbox next to the timestamp slider shows a 2-second interval of fixations around the selected timestamp. </p>
             <p> When the checkbox is unchecked it shows the cumulative fixations upto the selected timestamp. </p>
-        </div>
+       
 
         <div class="buttoncontainer" id="heatmap_rainbow">
             <p> Click this button to turn the heatmap colors to those of a rainbow. </p>
             <p> For optimal effect, please lower the intensity and radius. </p>
             <input type="button" id="rainbow_button" value="Toggle Rainbow">
         </div>
+    </div>
 
+        <p class="devider">Download</p>
 
-
-
-        <div id="downloadButtons">
+        <div id="downloadButtons" class="buttoncontainer">
             <div id="downloadVariables">
                 <input type="button" id="downloadVarsButton" value="Download variables file">
                 <a id="downloadVarsElement" style="display:none"></a>
