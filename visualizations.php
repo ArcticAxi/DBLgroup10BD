60896d44458b6c4c426fac581f4fc0aa6ce055c5
JennieAV <64147614+JennieAV@users.noreply.github.com>
2020-06-21 15:46:52 +0200

    fixed some sidebar problems

--- visualizations.php
+++ visualizations.php
@@ -24,7 +24,7 @@
                     <i class="fa fa-question" style="font-size:20px"></i></div></p>
                 <input type="range" min="1" max="10" value="1" class="slider" id="base_stroke_width_slider">
             </div>
-            <div id="highlight_stroke_width">
+            <div class="right_elem" id="highlight_stroke_width">
                 <p>Highlight Path Width:<div data-tooltip="Increase or decrease the width of highlighted paths for better clarity">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
@@ -37,7 +37,7 @@
                 </div></p>
                 <input type="range" min="0" max="10" value="7" class="slider" id="base_stroke_opacity_slider">
             </div>
-            <div id="highlight_stroke_opacity">
+            <div class="right_elem" id="highlight_stroke_opacity">
                 <p>Highlight Path Opacity:<div data-tooltip="Decrease if necessary to see both the background picture and the highlighted paths">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
@@ -50,7 +50,7 @@
                 </div></p>
                 <input type="range" min="0" max="15" value="3" class="slider" id="base_fixation_radius_slider">
             </div>
-            <div id="highlight_fixation_radius">
+            <div class="right_elem" id="highlight_fixation_radius">
                 <p>Highlight Fixation Size:<div data-tooltip = "Increase the size of individual highlighted fixations">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
@@ -63,7 +63,7 @@
                 </div></p>
                 <input type="range" min="0" max="10" value="7" class="slider" id="base_fixation_opacity_slider">
             </div>
-            <div id="highlight_fixation_opacity">
+            <div class="right_elem" id="highlight_fixation_opacity">
                 <p>Highlight Fixation Opacity:<div data-tooltip = "Decrease if necessary to see both the background picture and the highlighted fixations">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
@@ -80,7 +80,7 @@
 
         <p class="devider">Bubblemap</p>
         <div class="slidecontainer">
-            <div id="grid_size">
+            <div class="right_elem" id="grid_size">
                 <p> Change grid size: <div data-tooltip = "Increase for more and smaller bubbles, decrease for less and bigger bubbles">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
@@ -91,19 +91,19 @@
         <p class="devider">Heatmap</p>
 
         <div class="slidecontainer" id="all_heatmap_sliders">
-            <div id="intensity_heatmap">
+            <div class="right_elem" id="intensity_heatmap">
                 <p>Intensity:<div data-tooltip = "Slide to the left for more intense colors, slide to the right for less intense colors">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
                 <input type="range" min="1" max="30" value="5" class="slider" id="intensity_slider_heatmap">
             </div>
-            <div class="left_elem" id="radius_heatmap">
+            <div class="right_elem" class="left_elem" id="radius_heatmap">
                 <p>Radius:<div data-tooltip = "Increase or decrease size of each observation">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
                 <input type="range" min="1" max="10" value="6" class="slider" id="radius_slider_heatmap">
             </div>
-            <div id="blur_heatmap">
+            <div class="right_elem" id="blur_heatmap">
                 <p>Blur:<div data-tooltip = "Decrease the blur for more information on particular points or increase for a more general view">
                     <i class="fa fa-question" style="font-size:20px"></i>
                 </div></p>
